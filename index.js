2b08d76488ccdcdb690b1540e3fdc07275b63d7a
oldchevy <nick.olszowy@gmail.com>
2016-08-29 15:20:48 -0700

    Setting up server framework, adding snippet model and controller

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var app = require('express')();
-var partials = require('express-partials');
+//var partials = require('express-partials');
 var bodyParser = require('body-parser');
-var handler = require('./lib/handler');
+var snippetController = require('./lib/snippetController');
 var mongoose = require('mongoose');
+var morgan = require('morgan');
+var helpers = require('./lib/helpers');
 
 var port = 5000;
 
 app.listen(port);
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/data/snippets', snippetController.allSnippets);
+app.post('/data/snippets', snippetController.newSnippet);
+
+app.use(helpers.errorLogger);
+app.use(helpers.errorHandler);
 
 
 
